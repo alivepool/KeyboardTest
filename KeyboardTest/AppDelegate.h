54bfd153add3ac13d7d8d91c040d2e8c67a119d8
ameya.jathar <ameya.jathar@kony.com>
2016-07-07 14:03:42 +0530

    Initial Commit

---
+++ KeyboardTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyboardTest
+//
+//  Created by Ameya on 07/07/16.
+//  Copyright © 2016 Kony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeyboardTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeyboardTest
+//
+//  Created by Ameya on 07/07/16.
+//  Copyright © 2016 Kony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
